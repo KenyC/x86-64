e9fea8a14268e47a9e6204d9fd95b100cd44c169
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2019-12-05 10:48:57 -0300

    Update for GHC 8.6.5

--- CodeGen/X86/Examples.hs
+++ CodeGen/X86/Examples.hs
@@ -1,21 +1,25 @@
 {-# LANGUAGE FlexibleInstances #-}
 module CodeGen.X86.Examples where
 
-import Foreign
+import           Foreign
 
-import CodeGen.X86
+import           CodeGen.X86
 
 foreign import ccall "dynamic" callWW :: FunPtr (Word64 -> Word64) -> Word64 -> Word64
-instance Callable (Word64 -> Word64) where dynCCall = callWW
+instance Callable (Word64 -> Word64) where
+  dynCCall = callWW
 
 foreign import ccall "dynamic" callPW :: FunPtr (Ptr a -> Word64) -> Ptr a -> Word64
-instance Callable (Ptr a -> Word64) where dynCCall = callPW
+instance Callable (Ptr a -> Word64) where
+  dynCCall = callPW
 
 foreign import ccall "dynamic" callIO :: FunPtr (IO ()) -> IO ()
-instance Callable (IO ()) where dynCCall = callIO
+instance Callable (IO ()) where
+  dynCCall = callIO
 
 foreign import ccall "wrapper" createPtrWord64_Word64 :: (Word64 -> Word64) -> IO (FunPtr (Word64 -> Word64))
-instance CallableHs (Word64 -> Word64) where createHsPtr = createPtrWord64_Word64
+instance CallableHs (Word64 -> Word64) where
+  createHsPtr = createPtrWord64_Word64
 
 ------------------------------------------------------------------------------ 
 -- * examples
@@ -26,64 +30,64 @@ instance CallableHs (Word64 -> Word64) where createHsPtr = createPtrWord64_Word6
 --
 -- Output: @rax@
 idCode = do
-    mov result arg1
-    ret
+  mov result arg1
+  ret
 
 idFun :: Word64 -> Word64
-idFun = compile idCode 
+idFun = compile idCode
 
 -- | Example: Fibonacci function in Assembly
 fibCode = saveNonVolatile $ do
-    mov rdi arg1
-    inc rdi
-    xor_ rdx rdx
-    mov rax 1
-    doWhile NZ $ do
-        mov rcx rax
-        mov rax rdx
-        add rdx rcx
-        dec rdi
+  mov rdi arg1
+  inc rdi
+  xor_ rdx rdx
+  mov rax 1
+  doWhile NZ $ do
+    mov rcx rax
+    mov rax rdx
+    add rdx rcx
+    dec rdi
 
 fibFun :: Word64 -> Word64
-fibFun = compile fibCode 
+fibFun = compile fibCode
 
 -- | Example: trace a register in Assembly
 tracedFibCode = saveNonVolatile $ do
-    mov rdi arg1
-    inc rdi
-    xor_ rdx rdx
-    mov rax 1
-    doWhile NZ $ do
-        mov rcx rax
-        mov rax rdx
-        add rdx rcx
-        dec rdi
-        traceReg "d" rax
+  mov rdi arg1
+  inc rdi
+  xor_ rdx rdx
+  mov rax 1
+  doWhile NZ $ do
+    mov rcx rax
+    mov rax rdx
+    add rdx rcx
+    dec rdi
+    traceReg "d" rax
 
 tracedFibFun :: Word64 -> Word64
-tracedFibFun = compile tracedFibCode 
+tracedFibFun = compile tracedFibCode
 
 -- | Example: call Haskell @fib@ function from Assembly
 callHsCode = do
-    callFun r11 (hsPtr fib)
-    ret
+  callFun r11 (hsPtr fib)
+  ret
 
 fib :: Word64 -> Word64
 fib n = go n 0 1
-  where
-    go 0 a b = b `seq` a
-    go n a b = go (n-1) b (a+b)
+ where
+  go 0 a b = b `seq` a
+  go n a b = go (n - 1) b (a + b)
 
 callHsFun :: Word64 -> Word64
-callHsFun = compile callHsCode 
+callHsFun = compile callHsCode
 
 -- | Example: call C @printf@ function from Assembly
 --
 callCCode name = saveNonVolatile $ do
-    leaData arg1 $ CString "Hello %s!\n"
-    leaData arg2 $ CString name
-    xor_ rax rax                     -- zero XMM arguments ?????
-    callFun r11 printf
+  leaData arg1 $ CString "Hello %s!\n"
+  leaData arg2 $ CString name
+  xor_ rax rax                     -- zero XMM arguments ?????
+  callFun r11 printf
 
 callCFun :: String -> IO ()
 callCFun name = compile $ callCCode name
@@ -92,11 +96,11 @@ callCFun name = compile $ callCCode name
 
 memTestFun :: Word64 -> IO Bool
 memTestFun v = do
-    r <- mallocBytes 8                -- this is not required to be aligned (and in any case malloc aligns to machine words)
-    pokeByteOff r 0 (v :: Word64)
-    let code = saveNonVolatile $ do
-            mov rdi arg1
-            mov rax (addr rdi)
-    return $ compile code (r :: Ptr Word8) == v
+  r <- mallocBytes 8                -- this is not required to be aligned (and in any case malloc aligns to machine words)
+  pokeByteOff r 0 (v :: Word64)
+  let code = saveNonVolatile $ do
+        mov rdi arg1
+        mov rax (addr rdi)
+  return $ compile code (r :: Ptr Word8) == v
 
 
